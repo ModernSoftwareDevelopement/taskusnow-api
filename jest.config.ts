111534cae4e44a3c93fc873e4999e911f6abfd4d
thet naing <fatalthetnaing@gmail.com>
2023-08-16 11:04:18 +0800

    jest sonar configs and package json updates

--- jest.config.ts
+++ jest.config.ts
@@ -2,22 +2,20 @@ import type { Config } from "@jest/types";
 export default async (): Promise<Config.InitialOptions> => {
   return {
     preset: "ts-jest",
-     displayName: {
-     name: "taskusnow-api",
-     color: "greenBright",
-   },
-   verbose: true,
-   setupFiles: ["dotenv/config"],
-   testMatch: ["**/**/*.test.ts"],
-   testEnvironment: "node",
-   detectOpenHandles: true,
-   collectCoverage: true,
-   transform: { "^.+\\.tsx?$": "ts-jest" },
-   globalTeardown: "<rootDir>/src/tests/jest-globals-teardown.ts",
-   forceExit: true,
-   coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "<rootDir>/src/tests/**/*.test.ts",
-  ],
- };
+    displayName: {
+      name: "taskusnow-api",
+      color: "greenBright",
+    },
+    verbose: true,
+    setupFiles: ["dotenv/config"],
+    testMatch: ["**/**/*.test.ts"],
+    testEnvironment: "node",
+    detectOpenHandles: true,
+    collectCoverage: true,
+    transform: { "^.+\\.tsx?$": "ts-jest" },
+    globalTeardown: "<rootDir>/src/tests/jest-globals-teardown.ts",
+    forceExit: true,
+    testResultsProcessor: "jest-sonar-reporter",
+    coveragePathIgnorePatterns: ["/node_modules/", "/src/tests/"],
+  };
 };
