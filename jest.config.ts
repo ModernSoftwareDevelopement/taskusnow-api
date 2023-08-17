7d4adb886c3c31b01bc2d487d8338c9ceccc9a20
thet naing <fatalthetnaing@gmail.com>
2023-08-17 10:28:31 +0800

    rolled back the earlier change

--- jest.config.ts
+++ jest.config.ts
@@ -13,13 +13,13 @@ export default async (): Promise<Config.InitialOptions> => {
     detectOpenHandles: true,
     collectCoverage: true,
     transform: { "^.+\\.tsx?$": "ts-jest" },
-    globalTeardown: "<rootDir>/jest-globals-teardown.ts",
+    globalTeardown: "<rootDir>/src/Task/tests/jest-globals-teardown.ts",
     forceExit: true,
     testResultsProcessor: "jest-sonar-reporter",   
     collectCoverageFrom: [
       "src/**/*.{ts,tsx,js,jsx}",      
       "!src/**/*.d.ts",
-      "!src/**/**/*.ts",
+      "!src/Task/routes/*.ts",
       "!src/*.ts",
     ],    
   };
