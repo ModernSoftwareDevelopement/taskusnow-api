6b9e12c6dbb09d7179655c2f962bff1566e5628c
thet naing <fatalthetnaing@gmail.com>
2023-08-24 12:44:57 +0800

    fix : ignored jest.config.ts in test

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config.InitialOptions = {
   verbose: true,
   automock: false,
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/','jest.config.ts'],
   collectCoverage: true,
   coverageReporters: ['clover', 'json', 'lcov', ['text', { 'skipFull': true }]],
   coverageDirectory: 'coverage',
