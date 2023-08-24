b4f35b96457b0d2e6def53d7f5d1ca2a4cbcf075
thet naing <fatalthetnaing@gmail.com>
2023-08-24 12:36:41 +0800

    release : added our first test

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  automock: false,
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverage: true,
+  coverageReporters: ['clover', 'json', 'lcov', ['text', { 'skipFull': true }]],
+  coverageDirectory: 'coverage',
+};
+export default config;
