7e1fb29b2c1f3a6107f511970b89cbd23b635639
liucuxiu <xliucuxiux@gmail.com>
2023-09-05 08:14:56 +0700

    feat: use Auth0 for authentication

--- src/users/infra/routes/userRouter.ts
+++ src/users/infra/routes/userRouter.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from 'express';
 import { createUserController } from '../../useCases/createUser';
+import { validateJwt } from '../../authService/middleware/auth0.middleware';
 
 const userRouter = Router();
 
 userRouter.post('/users', async (req: Request, res: Response) => createUserController.execute(req, res));
+
+userRouter.get('/users-private', validateJwt, async (req: Request, res: Response) => {
+  res.send('user private');
+});
 
 export {
   userRouter
