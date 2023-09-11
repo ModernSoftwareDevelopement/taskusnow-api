efac5fafc287978539fd2119df7ac0dcdcc4e196
liucuxiu <xliucuxiux@gmail.com>
2023-09-11 20:50:10 +0700

    remove unused route

--- src/users/api/routes/userRouter.ts
+++ src/users/api/routes/userRouter.ts
@@ -1,15 +1,10 @@
 import { Request, Response, Router } from 'express';
 import { createUserController } from '../controllers';
-import { validateJwt } from '../../authService/middleware/auth0.middleware';
 
 const userRouter = Router();
 
-userRouter.post('/users', async (req: Request, res: Response) => createUserController.execute(req, res));
+userRouter.post('/users', async (req: Request, res: Response) =>
+  createUserController.execute(req, res),
+);
 
-userRouter.get('/users-private', validateJwt, async (req: Request, res: Response) => {
-  res.send('user private');
-});
-
-export {
-  userRouter
-};
+export { userRouter };
