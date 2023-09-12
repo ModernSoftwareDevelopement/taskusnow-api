b7c95592a547510b9d4f6b065b0b4eddc456ee58
liucuxiu <xliucuxiux@gmail.com>
2023-09-12 09:19:46 +0700

    feat: add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 import { userRouter } from './users/api/routes/userRouter';
+import helmet from 'helmet';
 
 const app: Express = express();
 
+app.use(helmet());
 app.use(express.json());
 
 app.use(userRouter);
