3a558f365b8939c30476b2fd01458006940c1ce3
liucuxiu <xliucuxiux@gmail.com>
2023-09-12 09:24:08 +0700

    chore: remove unused code

--- src/users/api/routes/userRouter.ts
+++ src/users/api/routes/userRouter.ts
@@ -4,11 +4,7 @@ import { createUserController } from '../controllers';
 const userRouter = Router();
 
 userRouter.post('/users', (req: Request, res: Response) => {
-  createUserController.execute(req, res).catch((err: unknown) => {
-    res.status(500).json({
-      message: (err as Error).message, //fix this to common error message later
-    });
-  });
+  createUserController.execute(req, res).catch();
 });
 
 export { userRouter };
