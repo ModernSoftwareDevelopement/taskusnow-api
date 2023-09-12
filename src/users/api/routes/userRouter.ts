dad5876049681efb69cdf0e13546a5f82c0c74b4
liucuxiu <xliucuxiux@gmail.com>
2023-09-12 09:08:02 +0700

    chore: change userRoute to use promise

--- src/users/api/routes/userRouter.ts
+++ src/users/api/routes/userRouter.ts
@@ -3,8 +3,12 @@ import { createUserController } from '../controllers';
 
 const userRouter = Router();
 
-userRouter.post('/users', async (req: Request, res: Response) =>
-  createUserController.execute(req, res),
-);
+userRouter.post('/users', (req: Request, res: Response) => {
+  createUserController.execute(req, res).catch((err: unknown) => {
+    res.status(500).json({
+      message: (err as Error).message, //fix this to common error message later
+    });
+  });
+});
 
 export { userRouter };
