19ae7e03549e714ba5ab8cc1d7edaf8eb083d969
thet naing <fatalthetnaing@gmail.com>
2023-09-18 17:47:55 +0800

    fix: trying to fix sonar bug Promise return void

--- src/Task/api/routes/taskRoutes.ts
+++ src/Task/api/routes/taskRoutes.ts
@@ -4,30 +4,20 @@ import {
   getTasksController,
   getTaskbyTaskIDController,
 } from '../controllers';
-import { asyncMiddleware } from '../../../middleware/async';
 
 export const setupTaskRoutes = () => {
   const router = Router();
 
-  router.get(
-    '/tasks',
-    asyncMiddleware(async (req: Request, res: Response) => {
-      getTasksController.execute(req, res).catch();
-    }),
-  );
+  router.get('/tasks', (req: Request, res: Response) => {
+    getTasksController.execute(req, res).catch();
+  });
 
-  router.post(
-    '/task',
-    asyncMiddleware(async (req: Request, res: Response) => {
-      createTaskController.execute(req, res).catch();
-    }),
-  );
+  router.post('/task', (req: Request, res: Response) => {
+    createTaskController.execute(req, res).catch();
+  });
 
-  router.get(
-    '/task',
-    asyncMiddleware(async (req: Request, res: Response) => {
-      getTaskbyTaskIDController.execute(req, res).catch();
-    }),
-  );
+  router.get('/task', (req: Request, res: Response) => {
+    getTaskbyTaskIDController.execute(req, res).catch();
+  });
   return router;
 };
