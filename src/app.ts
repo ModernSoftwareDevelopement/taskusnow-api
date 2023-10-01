73d8e002f786a094fa8922004b212dcc9bc222e2
liucuxiu <xliucuxiux@gmail.com>
2023-10-01 22:35:52 +0700

    feat: upload image with cloudinary (controller)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import { userRouter } from './users/api/routes/userRouter';
 import helmet from 'helmet';
+import { profileRouter } from './users/api/routes/profileRouter';
 
 const app: Express = express();
 
@@ -8,6 +9,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.use(userRouter);
+app.use(profileRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server!');
---
+++ src/users/api/routes/profileRouter.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import multer from 'multer';
+import { uploadProfileImageController } from '../controllers/uploadProfileImage';
+
+const profileRouter = Router();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+  },
+});
+
+profileRouter.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+profileRouter.post('/profile/upload', upload.single('image'), (req, res) => {
+  uploadProfileImageController.execute(req, res).catch();
+});
+
+export { profileRouter };
