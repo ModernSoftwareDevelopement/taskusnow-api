946dea35fe2935ee39edc5d651ecdbfeeaccacdc
liucuxiu <xliucuxiux@gmail.com>
2023-10-01 22:53:30 +0700

    chore: change file upload limit

--- src/users/api/routes/profileRouter.ts
+++ src/users/api/routes/profileRouter.ts
@@ -7,7 +7,7 @@ const profileRouter = Router();
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+    fileSize: 2 * 1024 * 1024, // limit file size to 5MB
   },
 });
 
