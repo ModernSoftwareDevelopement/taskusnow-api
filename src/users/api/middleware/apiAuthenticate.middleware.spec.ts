3ed3c8561b7f245e34f8467d92dab7826cfa5053
liucuxiu <xliucuxiux@gmail.com>
2023-10-18 20:48:31 +0700

    feat: add authentication middleware for "create user API"

---
+++ src/users/api/middleware/apiAuthenticate.middleware.spec.ts
@@ -0,0 +1,72 @@
+import { Request, Response, NextFunction } from 'express';
+import { apiAuthenticateMiddleware } from './apiAuthenticate.middleware';
+import dotenv from 'dotenv';
+
+describe('apiAuthenticateMiddleware', () => {
+  beforeAll(() => {
+    dotenv.config();
+    process.env.API_USERNAME = 'your_username';
+    process.env.API_PASSWORD = 'your_password';
+  });
+
+  afterAll(() => {
+    delete process.env.API_USERNAME;
+    delete process.env.API_PASSWORD;
+  });
+  it('should return a 401 response if the "authorization" header is missing', () => {
+    const req = {
+      headers: {},
+    } as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn() as unknown as NextFunction;
+
+    apiAuthenticateMiddleware(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Unauthorized' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it('should return a 401 response if the provided token is incorrect', () => {
+    const req = {
+      headers: { authorization: 'InvalidToken' },
+    } as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn() as unknown as NextFunction;
+
+    apiAuthenticateMiddleware(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Unauthorized' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it('should call next() if the provided token is correct', () => {
+    const username = process.env.API_USERNAME || '';
+    const password = process.env.API_PASSWORD || '';
+    const authHeader =
+      'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+
+    const req = {
+      headers: { authorization: authHeader },
+    } as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+    const next = jest.fn() as unknown as NextFunction;
+
+    apiAuthenticateMiddleware(req, res, next);
+
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+});
--- src/users/api/routes/userRouter.ts
+++ src/users/api/routes/userRouter.ts
@@ -1,12 +1,17 @@
 import { Request, Response, Router } from 'express';
 import { createUserController } from '../controllers/createUser';
 import { getUserByIdController } from '../controllers/getUserById';
+import { apiAuthenticateMiddleware } from '../middleware/apiAuthenticate.middleware';
 
 const userRouter = Router();
 
-userRouter.post('/users', (req: Request, res: Response) => {
-  createUserController.execute(req, res).catch();
-});
+userRouter.post(
+  '/users',
+  apiAuthenticateMiddleware,
+  (req: Request, res: Response) => {
+    createUserController.execute(req, res).catch();
+  },
+);
 
 userRouter.get('/users/:id', (req: Request, res: Response) => {
   getUserByIdController.execute(req, res).catch();
