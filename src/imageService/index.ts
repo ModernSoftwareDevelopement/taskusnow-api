2846ffeef6fdc1536119a37d3dce5a4da5219f52
liucuxiu <xliucuxiux@gmail.com>
2023-10-18 21:03:42 +0700

    chore: remove unused import

--- src/imageService/index.ts
+++ src/imageService/index.ts
@@ -1,7 +1,5 @@
 import { CloudinaryImageUploader } from './CloudinaryImageUploader';
-import { S3ImageUploader } from './S3ImageUploader';
 
 const cloudinaryImageUploader = new CloudinaryImageUploader();
-const s3ImageUploader = new S3ImageUploader('liucuxiu-user-profile-images');
 
-export { cloudinaryImageUploader, s3ImageUploader };
+export { cloudinaryImageUploader };
