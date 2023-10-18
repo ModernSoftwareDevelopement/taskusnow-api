22a11aeb67c68eae7e6a8c4917237c28fa3fcc61
liucuxiu <xliucuxiux@gmail.com>
2023-10-18 09:26:41 +0700

    refactor: rename usecase folder

--- src/users/api/controllers/uploadProfileImage/UploadProfileImageController.ts
+++ src/users/api/controllers/uploadProfileImage/UploadProfileImageController.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 import { ImageUploader } from '../../../../imageService/ImageUploader';
+import { UpdateUserProfileUseCase } from '../../../useCases/uploadUserProfile/UpdateUserProfileUseCase';
 
 export class UploadProfileImageController {
-  constructor(private readonly imageUploader: ImageUploader) {}
+  constructor(
+    private readonly imageUploader: ImageUploader,
+    private readonly updateUseProfileUseCase: UpdateUserProfileUseCase,
+  ) {}
 
   async execute(req: Request, res: Response): Promise<Response> {
     if (!req.file) {
@@ -11,11 +15,17 @@ export class UploadProfileImageController {
       });
     }
 
+    const userId = req.params.userId || '';
+
     const { originalname, buffer } = req.file;
 
-    const result = await this.imageUploader.uploadImage(originalname, buffer);
+    console.log('payload', req.auth);
 
-    //uploadProfileImageUseCase.execute(originalname);
+    const imageUrl = await this.imageUploader.uploadImage(originalname, buffer);
+
+    const result = await this.updateUseProfileUseCase.execute(userId, {
+      imageUrl,
+    });
 
     return res.status(201).json({
       message: result,
