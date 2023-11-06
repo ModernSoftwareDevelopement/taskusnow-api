8dbd0d01de837cd464b75ba40216917e643e8b4b
liucuxiu <xliucuxiux@gmail.com>
2023-11-06 07:59:33 +0700

    feat: add mongo database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import { userRouter } from './users/api/routes/userRouter';
 import helmet from 'helmet';
+import cors from 'cors';
 import { profileRouter } from './users/api/routes/profileRouter';
+import mongoose from 'mongoose';
 
 const app: Express = express();
 
+const mongoUri: string = 'mongodb://localhost:27017/taskusnow';
+mongoose.connect(mongoUri).then(() => {
+  console.log('connected Mongo!');
+});
+
 app.use(helmet());
+
+app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
