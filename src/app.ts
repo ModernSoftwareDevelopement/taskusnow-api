66d5b3e613b8230370bc5a5d2fad53be08fcd979
banyarthein <banyarthein@gmail.com>
2023-11-07 23:40:16 +0800

    Fixed User Issue while push

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express, { Express, Request, Response } from 'express';
 import helmet from 'helmet';
 import { setupTaskRoutes } from './Task/api/routes/taskRoutes';
 import { postRouter } from './Post/api/routes/postRouter';
+import { userRouter } from './users/api/routes/userRouter';
 
 const app: Express = express();
 
 app.use(helmet());
 app.use(express.json());
 
+app.use('/api/user', userRouter);
 app.use('/api/task', setupTaskRoutes());
 app.use('/api/posts', postRouter);
 
--- src/users/api/routes/userRouter.spec.ts
+++ src/users/api/routes/userRouter.spec.ts
@@ -12,7 +12,7 @@ describe('User Router', () => {
   it('should call create user controller when call POST /users', async () => {
     const mockExecute = jest.spyOn(createUserController, 'execute');
 
-    await request(app).post('/users').send({
+    await request(app).post('/').send({
       email: 'liucuxiu@gmail.com',
     });
 
@@ -22,7 +22,7 @@ describe('User Router', () => {
   it('should call get user by id controller when call GET /users/:id', async () => {
     const mockExecute = jest.spyOn(getUserByIdController, 'execute');
 
-    await request(app).get('/users/id');
+    await request(app).get('/id');
 
     expect(mockExecute).toBeCalledTimes(1);
   });
