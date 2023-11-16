174c3bd5cbdc3a04882502677384739200b54c30
banyarthein <banyarthein@gmail.com>
2023-11-16 22:44:59 +0800

    commit app file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from 'express';
 import helmet from 'helmet';
-import { setupTaskRoutes } from './Task/api/routes/taskRoutes';
 import { postRouter } from './Post/api/routes/postRouter';
 import { userRouter } from './users/api/routes/userRouter';
 import { profileRouter } from './users/api/routes/profileRouter';
